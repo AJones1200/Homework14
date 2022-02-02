b5df8e688b04334ce720b80ec0e8bc8ab767fdb4
Ayse Jones <aysej305@gmail.com>
2022-02-02 15:25:22 -0600

    comments somewhat working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -29,17 +29,21 @@ router.get('/', async (req, res) => {
 
 router.get('/post/:id', async (req, res) => {
   try {
+    console.log("routehit");
     const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
           attributes: ['name'],
         },
+        {
+          model: Comment,
+          include: [User],
+        },
       ],
     });
-
     const post = postData.get({ plain: true });
-
+console.log(post)
     res.render('post', {
       ...post,
       logged_in: req.session.logged_in
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Comment = require('./Comment');
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
-});
+})
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
